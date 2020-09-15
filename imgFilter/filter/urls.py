1a8d8f4a5273af8e5a344a81b7a9f800b3d3cb4f
“xiang2002 <2014176995@qq.com>
2020-09-16 01:41:21 +0800

    django

--- imgFilter/filter/urls.py
+++ imgFilter/filter/urls.py
@@ -4,5 +4,9 @@ from filter import views
 app_name = 'filter'
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("logout/", views.logout, name="logout"),
+    path("del/", views.delete, name="delete"),
+    path("move/", views.move, name="move"),
+    path("save/", views.save, name="save"),
 ]
