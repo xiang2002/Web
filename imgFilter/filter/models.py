5c01388c64e469d692dc9ceaf5390870362c5007
“xiang2002 <2014176995@qq.com>
2020-09-16 01:40:59 +0800

    django

--- imgFilter/filter/models.py
+++ imgFilter/filter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Queue(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=20, default='0')
+
+    class Meta:
+        db_table = 'queue'
+        verbose_name = "筛选队列"
+        verbose_name_plural = "筛选队列"
