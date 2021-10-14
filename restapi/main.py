05ebf866552a1d3ea922c7c9293a01b7e791dcc6
向善 <58551590+xiang2002@users.noreply.github.com>
2021-10-14 20:23:04 +0800

    Add files via upload

--- restapi/main.py
+++ restapi/main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask,request
 from flask.views import MethodView
 from google.cloud import ndb
+import json
 
 
 class Boat(ndb.Model):
@@ -8,35 +9,40 @@ class Boat(ndb.Model):
     name = ndb.StringProperty(required=True)
     type = ndb.StringProperty()
     length = ndb.IntegerProperty()
-    at_sea = ndb.BooleanProperty()
-
 app = Flask(__name__)
-
-class UserAPI(MethodView):
-    
-    def get(self, user_id):
-        if user_id is None:
-            return "user_id None"
+class BoatAPI(MethodView):
+    def get(self, boat_id):
+        if boat_id is None:
+            return "boat_id None"
         else:
-            return "User _id"+str(user_id)
-
+            return "boat_id"+str(boat_id)
     def post(self):
-        # create a new user
-        pass
+        boat_data = json.loads(request.get_data(as_text=True))
+        new_boat = Boat(name=boat_data['name'])
+        if boat_data.get('type'):
+            new_boat.type = boat_data['type']
+        if boat_data.get('length'):
+            new_boat.length = boat_data['length']
+        new_boat.put()
+        new_boat.id = new_boat.key.urlsafe()
+        new_boat.put()
+        boat_dict = new_boat.to_dict()
+        boat_dict['self'] = '/boat/' + new_boat.id
+        return json.dumps(boat_dict)
 
-    def delete(self, user_id):
+    def delete(self, boat_id):
         # delete a single user
         pass
 
-    def put(self, user_id):
+    def put(self, boat_id):
         # update a single user
         pass
 
-user_view = UserAPI.as_view('user_api')
-app.add_url_rule('/users/', defaults={'user_id': None},
-                 view_func=user_view, methods=['GET',])
+boat_view = BoatAPI.as_view('boat_api')
+app.add_url_rule('/boats/', defaults={'boat_id': None},
+                 view_func=boat_view, methods=['GET',])
 # app.add_url_rule('/users/', view_func=user_view, methods=['POST',])
-# app.add_url_rule('/users/<int:user_id>', view_func=user_view,
-#                  methods=['GET', 'PUT', 'DELETE'])
+app.add_url_rule('/boats/<int:boat_id>', view_func=boat_view,
+                 methods=['GET', 'PUT', 'DELETE'])
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
